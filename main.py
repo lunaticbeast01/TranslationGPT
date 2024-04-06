58a35ecbb39cad7b33a4a225aae480625f9da0f8
Ashutosh Anand Prakash <128904048+lunaticbeast01@users.noreply.github.com>
2024-04-06 22:26:34 +0530

    minor change

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class MyClient(discord.Client):
         if self.user != message.author:
             if self.user in message.mentions:
                 channel = message.channel
-                prefix = "Please ignore all previous instructions. Please       respond only in the hindi, Gujrati, tamil language. Do not explain what you are doing. Do not self reference. You are an expert translator. Translate the following text to the the hindi, Gujrati, tamil using vocabulary and expressions of a native of India. The text to be translated is \""
+                prefix = "Please ignore all previous instructions. Please respond only in the hindi, Gujrati, tamil language. Do not explain what you are doing. Do not self reference. You are an expert translator. Translate the following text to the the hindi, Gujrati, tamil using vocabulary and expressions of a native of India. The text to be translated is \""
                 suffix = "\""
                 response = openai_client.completions.create(
                     model="gpt-3.5-turbo-instruct-0914",
